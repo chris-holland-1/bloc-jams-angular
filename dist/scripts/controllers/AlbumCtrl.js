251c6816db97ca7f090b73c50f7e22dd47fa0122
Chris Holland <chris.holland987@gmail.com>
2016-12-30 21:06:47 -0800

    Checkpoint 7 Services 2: completed

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,11 @@
 (function() {
-    function AlbumCtrl(Fixtures) {
+    function AlbumCtrl(Fixtures, SongPlayer) {
         console.log("controller loaded!");
-        this.albumData = Fixtures.getAlbum();  
+        this.albumData = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
     }
     
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
