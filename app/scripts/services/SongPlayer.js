82bda57604891da35eb03024fa1f31c254625c6b
Chris Holland <chris.holland987@gmail.com>
2017-01-01 16:58:09 -0800

    Assignment 7 Services 2: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,13 +1,24 @@
 (function() {
     function SongPlayer() {
+        
+         /**
+         * @desc Service will return this object, making its properties & methods public to the rest of the application
+         * @type {Object}
+        */
+        
         var SongPlayer = {};
+        
+        /**
+        * @desc The song currently being referenced for an action - play or pause
+        * @type {Object}
+        */
         
         var currentSong = null;
         
         /**
         * @desc Buzz object audio file
         * @type {Object}
-        */
+        */ 
         
         var currentBuzzObject = null;
         
@@ -28,6 +39,19 @@
             });
             
             currentSong = song;
+        };
+        
+         var playSong = function(song) {
+            if (currentBuzzObject) {
+                currentBuzzObject.play();
+                currentSong.playing = !null;
+            }
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
+            
+            song.playing = true;
         };
         
         SongPlayer.play = function(song) {
